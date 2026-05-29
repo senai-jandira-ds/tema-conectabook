23bfdf83c8c14c5ed8b0ca3d1a886a8b984e4f42
isabelleabreu <isa.estudos597@gmail.com>
2026-05-29 15:54:59 -0300

    Tela de criar um clube finalizada

---
+++ app/src/main/java/com/example/conectabook/screens/FeedClubeScreen.kt
@@ -0,0 +1,10 @@
+package com.example.conectabook.screens
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun FeedClubeScreen(modifier: Modifier = Modifier) {
+    
+}
